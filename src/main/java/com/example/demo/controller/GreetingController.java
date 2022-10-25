42d84fc0d34f868d578d05b3b7c421b40fb9d9bc
Nícolas Félix <nf.oliveira@hotmail.com>
2022-10-25 00:39:28 -0300

    Anotação end-point

--- src/main/java/com/example/demo/controller/GreetingController.java
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -14,6 +14,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
 
+    @RequestMapping("/greeting")
     public Greeting greeting(
             @RequestParam(value = "name", defaultValue = "world") String name) {
             return new Greeting(counter.incrementAndGet(), String.format(template, name));
